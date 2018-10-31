3921d015f804a47c73ecafca50f09421a058aee6
Maximilian Petri <maximilian.petri2000@gmail.com>
2018-10-31 16:01:54 +0100

    added first GUI elements

---
+++ src/de/petri/onu/client/GUI.java
@@ -0,0 +1,171 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Point;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GUI extends JFrame {
+
+	private JPanel contentPane;
+	
+	public JPanel contentPanel;
+	
+	Menu menu;
+	Lobby lobby;
+	
+	private Point initialClick;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI() {
+		setUndecorated(true);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JPanel TopBar = new JPanel();
+		TopBar.setBounds(0, 0, 800, 50);
+		TopBar.addMouseListener(new MouseAdapter() {
+	        public void mousePressed(MouseEvent e) {
+	            initialClick = e.getPoint();
+	            getComponentAt(initialClick);
+	        }
+	    });
+
+	    TopBar.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+	        public void mouseDragged(MouseEvent e) {
+
+	            // get location of Window
+	            int thisX = getLocation().x;
+	            int thisY = getLocation().y;
+
+	            // Determine how much the mouse moved since the initial click
+	            int xMoved = e.getX() - initialClick.x;
+	            int yMoved = e.getY() - initialClick.y;
+
+	            // Move window to this position
+	            int X = thisX + xMoved;
+	            int Y = thisY + yMoved;
+	            setLocation(X, Y);
+	        }
+	    });
+		contentPane.setLayout(null);
+		
+		
+		TopBar.setBackground(Color.DARK_GRAY);
+		contentPane.add(TopBar);
+		TopBar.setLayout(null);
+		
+		JButton btnMin = new JButton("-");
+		btnMin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setState(ICONIFIED);
+			}
+		});
+		btnMin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnMin.setOpaque(true);
+				btnMin.setContentAreaFilled(true);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnMin.setOpaque(false);
+				btnMin.setContentAreaFilled(false);
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+		});
+		btnMin.setBounds(690, 0, 50, 50);
+		TopBar.add(btnMin);
+		btnMin.setOpaque(false);
+		btnMin.setForeground(Color.WHITE);
+		btnMin.setFont(new Font("Calibri", Font.BOLD, 55));
+		btnMin.setFocusPainted(false);
+		btnMin.setContentAreaFilled(false);
+		btnMin.setBorderPainted(false);
+		btnMin.setBorder(null);
+		btnMin.setBackground(Color.GRAY);
+		
+		JButton btnClose = new JButton("X");
+		btnClose.setBounds(750, 0, 50, 50);
+		TopBar.add(btnClose);
+		btnClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnClose.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnClose.setOpaque(true);
+				btnClose.setContentAreaFilled(true);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnClose.setOpaque(false);
+				btnClose.setContentAreaFilled(false);
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+		});
+		btnClose.setOpaque(false);
+		btnClose.setForeground(Color.WHITE);
+		btnClose.setFont(new Font("Arial Black", Font.BOLD, 24));
+		btnClose.setFocusPainted(false);
+		btnClose.setContentAreaFilled(false);
+		btnClose.setBorderPainted(false);
+		btnClose.setBorder(null);
+		btnClose.setBackground(Color.RED);
+		
+		contentPanel = new JPanel();
+		contentPanel.setBackground(Color.LIGHT_GRAY);
+		contentPanel.setBounds(0, 50, 800, 550);
+		contentPanel.setLayout(null);
+		
+		menu = new Menu(this);
+		menu.setForeground(new Color(255, 255, 255));
+		menu.setBounds(0, 0, 800, 550);
+		contentPanel.add(menu);
+		contentPane.add(contentPanel);
+		
+	}
+	
+}
---
+++ src/de/petri/onu/client/Menu.java
@@ -0,0 +1,121 @@
+package gui;
+
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+
+import net.miginfocom.swing.MigLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Cursor;
+import javax.swing.border.LineBorder;
+
+public class Menu extends JPanel {
+
+	GUI gui;
+	
+	Menu itsMe;
+	
+	public Menu(GUI gui) {
+		this.gui = gui;
+		
+		itsMe = this;
+		
+		setBackground(new Color(255, 255, 102));
+		setBounds(0, 0, 800, 550);
+		
+		JButton btnLobby = new JButton("Lobby");
+		btnLobby.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnLobby.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnLobby.setFont(new Font("Calibri", Font.BOLD, 20));
+		btnLobby.setFocusTraversalKeysEnabled(false);
+		btnLobby.setFocusPainted(false);
+		btnLobby.setBackground(Color.GRAY);
+		btnLobby.setBorderPainted(false);
+		btnLobby.setBorder(null);
+		btnLobby.setFocusable(false);
+		btnLobby.setBounds(250, 252, 300, 50);
+		btnLobby.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		setLayout(null);
+		add(btnLobby);
+		
+		JButton btnSettings = new JButton("Einstellungen");
+		btnSettings.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnSettings.setActionCommand("");
+		btnSettings.setEnabled(false);
+		btnSettings.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnSettings.setFont(new Font("Calibri", Font.BOLD, 20));
+		btnSettings.setFocusable(false);
+		btnSettings.setFocusTraversalKeysEnabled(false);
+		btnSettings.setFocusPainted(false);
+		btnSettings.setBorderPainted(false);
+		btnSettings.setBorder(null);
+		btnSettings.setBackground(Color.GRAY);
+		btnSettings.setBounds(250, 342, 300, 50);
+		add(btnSettings);
+		
+		JButton btnQuit = new JButton("Beenden");
+		btnQuit.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnQuit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(1);
+			}
+		});
+		btnQuit.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnQuit.setFont(new Font("Calibri", Font.BOLD, 20));
+		btnQuit.setFocusable(false);
+		btnQuit.setFocusTraversalKeysEnabled(false);
+		btnQuit.setFocusPainted(false);
+		btnQuit.setBorderPainted(false);
+		btnQuit.setBorder(null);
+		btnQuit.setBackground(Color.GRAY);
+		btnQuit.setBounds(250, 432, 300, 50);
+		add(btnQuit);
+		
+		JButton btnJoin = new JButton("Beitreten");
+		btnJoin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				gui.contentPanel.setVisible(false);
+				gui.contentPanel.removeAll();
+				gui.contentPanel.add(new Lobby(gui));
+				gui.contentPanel.setVisible(true);
+			}
+		});
+		btnJoin.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnJoin.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnJoin.setFont(new Font("Calibri", Font.BOLD, 20));
+		btnJoin.setFocusable(false);
+		btnJoin.setFocusTraversalKeysEnabled(false);
+		btnJoin.setFocusPainted(false);
+		btnJoin.setBorderPainted(false);
+		btnJoin.setBorder(null);
+		btnJoin.setBackground(Color.GRAY);
+		btnJoin.setBounds(250, 162, 300, 50);
+		add(btnJoin);
+		
+		JLabel lblOnu = new JLabel("Onu");
+		lblOnu.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+		lblOnu.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		lblOnu.setForeground(new Color(204, 0, 0));
+		lblOnu.setFont(new Font("Tahoma", Font.BOLD, 40));
+		lblOnu.setHorizontalAlignment(SwingConstants.CENTER);
+		lblOnu.setBounds(250, 27, 300, 70);
+		add(lblOnu);
+
+	}
+}
